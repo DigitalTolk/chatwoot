f7569962b9e28cfa35ddc269a8ba5c3d4d5fef10
John Manuel Derecho <jmanuel.derecho@gmail.com>
2024-02-14 02:28:21 +0800

    fix

--- app/jobs/migration/fix_csat_on_replies.rb
+++ app/jobs/migration/fix_csat_on_replies.rb
@@ -4,14 +4,14 @@ class Migration::FixCsatOnReplies < ApplicationJob
   def perform
     inbox_ids = Inbox.where(csat_survey_enabled: true, csat_trigger: 'conversation_all_reply')
 
-    puts "csat count: #{Message.count}"
+    puts "csat_count: #{Message.count}"
     Conversation.where(inbox_id: inbox_ids).each do |convo|
+      next if convo.messages.outgoing.blank?
       template = convo.inbox.csat_template
       next if convo.messages.csat.count >= template.questions_count
 
-      # fix
       MessageTemplates::Template::CsatSurvey.new(conversation: convo).perform
     end
-    puts "csat count: #{Message.count}"
+    puts "csat_count: #{Message.count}"
   end
 end
