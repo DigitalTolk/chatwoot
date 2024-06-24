d1b9ed40687f3d5c5c117306593aaf5c423a2ea6
John Manuel Derecho <jmanuel.derecho@gmail.com>
2024-06-24 17:28:57 +0800

    remove explicit definition for csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
+# Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
@@ -16,10 +16,7 @@ Rails.application.config.content_security_policy do |policy|
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
-
-  # allow digitaltolk.se or www.digitaltolk.se to be embedded in an iframe
-  policy.frame_src :self, 'https://digitaltolk.se', 'https://www.digitaltolk.se'
-end
+# end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
